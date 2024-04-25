0b7b4ac417a4cbe559de1218ecc998c5b3e93138
Faisal Aylab <faisal@aylab.io>
2024-04-25 11:52:16 +0500

    components setup and initial basic landing

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,31 @@
-import { Text } from "@chakra-ui/react";
+import Header from "@/components/Header";
+import { Box, Image } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-      <>
-        <Text fontSize={'50px'} color={'#121211'}>Hello</Text>
-      </>
+    <Box
+      position="relative" // Needed for absolute positioning context
+      bgImage="url('/assets/pngs/bg-sky.png')"
+      bgPos="center"
+      bgSize="cover"
+      bgRepeat="no-repeat"
+      h="145vh"
+      w="full"
+    >
+      <Header />
+      {/* Balloon Image */}
+      <Image
+        src="/assets/svgs/balloon.svg"
+        alt="Balloon"
+        position="relative"
+        left="50%" 
+        top="45%" 
+        transform="translate(-50%, -50%)" 
+        zIndex="0" 
+        // Apply float animation here
+      />
+
+      {/* Other content */}
+    </Box>
   );
 }
